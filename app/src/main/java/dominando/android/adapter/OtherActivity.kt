a5623ee6a9022206a1bf3d23c1bdfd28278ef318
Vinicius Rio <viniciustrave@gmail.com>
2023-03-25 12:44:54 -0300

    Add intent

---
+++ app/src/main/java/dominando/android/adapter/OtherActivity.kt
@@ -0,0 +1,15 @@
+package dominando.android.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+        val name = intent.getStringExtra("name");
+        tvMessage.text = name
+    }
+}
