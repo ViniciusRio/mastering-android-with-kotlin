eee590415bc91134654fdc001560851fdd38ee90
Vinicius Rio <viniciustrave@gmail.com>
2023-03-25 18:27:40 -0300

    Reformatted

--- app/src/main/java/dominando/android/adapter/OtherActivity.kt
+++ app/src/main/java/dominando/android/adapter/OtherActivity.kt
@@ -1,7 +1,7 @@
 package dominando.android.adapter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_other.*
 import org.parceler.Parcels
 
@@ -13,18 +13,19 @@ class OtherActivity : AppCompatActivity() {
         val name = intent.getStringExtra("name");
         val age = intent.getIntExtra("age", -1);
         val client = intent.getParcelableExtra<Client>("client")
-        val clientParceler = Parcels.unwrap<ClientParceler?>(intent.getParcelableExtra<Client>("clientParcel"))
+        val clientParceler =
+            Parcels.unwrap<ClientParceler?>(intent.getParcelableExtra<Client>("clientParcel"))
         val person = intent.getSerializableExtra("person") as Person?
 
-        tvMessage.text = if(client != null) {
+        tvMessage.text = if (client != null) {
             getString(R.string.text_one_other_activity, client.name, client.code)
-        } else if (person !=null ) {
-            getString(R.string.text_two_other_activity, person?.name, person?.age)
-        }
-        else if (clientParceler !=null ) {
-                getString(R.string.text_one_other_activity, clientParceler.name,
-                    clientParceler.code
-                )
+        } else if (person != null) {
+            getString(R.string.text_two_other_activity, person.name, person.age)
+        } else if (clientParceler != null) {
+            getString(
+                R.string.text_one_other_activity, clientParceler.name,
+                clientParceler.code
+            )
         } else {
             getString(R.string.text_two_other_activity, name, age)
         }
